364bf87c5885e9fd5af772da0f89613083af07b3
arminhabiban1 <132064497+arminhabiban1@users.noreply.github.com>
2024-02-11 10:27:10 +1100

    add background to main page using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import Navbar from './component/Navbar/Navbar';
 import Background from './component/Background/Background';
  import Hero from './component/Hero/Hero';
